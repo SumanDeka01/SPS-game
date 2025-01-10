9ecb596b2a48a62b3bd7c7e75344bb96077108a9
SumanDeka01 <sdeka2903@gmail.com>
2025-01-10 12:32:51 +0530

    Add images foler

--- RPS.js
+++ RPS.js
@@ -6,71 +6,62 @@ const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#Comp-score");
 
-
 const drawGame = () => {
-    console.log("The gave was draw.");
-    msg.innerHTML = "Gave was draw! Play again.";
-    msg.style.background = "#081b31";
-
-
-}
-
-
+  console.log("The gave was draw.");
+  msg.innerHTML = "Gave was draw! Play again.";
+  msg.style.background = "#081b31";
+};
 
 const showWinner = (userWin, userChoice, compChoice) => {
-    if(userWin) {
-        userScore++;
-        userScorePara.innerHTML= userScore;
+  if (userWin) {
+    userScore++;
+    userScorePara.innerHTML = userScore;
 
-        msg.innerHTML = 'YOU WIN!';
-        msg.style.background = "green";
-    } else {
-        CompScore++;
-        compScorePara.innerHTML= CompScore;
+    msg.innerHTML = "YOU WIN!";
+    msg.style.background = "green";
+  } else {
+    CompScore++;
+    compScorePara.innerHTML = CompScore;
 
-        msg.innerHTML = "YOU LOSE! ";
-        msg.style.background = "red";
-
-    }
-}
+    msg.innerHTML = "YOU LOSE! ";
+    msg.style.background = "red";
+  }
+};
 
 const genCompChoice = () => {
-    const options = ["rock", "paper" , "scissors"];
-    const randIdx = Math.floor(Math.random() * 3);
-    return options[randIdx];
-}
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
 
 const playGame = (userChoice) => {
-    console.log("user choice =", userChoice)
-    const compChoice = genCompChoice();
-    console.log("comp choice =", compChoice)
+  console.log("user choice =", userChoice);
+  const compChoice = genCompChoice();
+  console.log("comp choice =", compChoice);
 
-    if(userChoice === compChoice) {
-        drawGame();
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      //scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      compChoice === "scissors" ? false : true;
     } else {
-        let userWin = true;
-        if(userChoice === "rock") {
-            //scissors, paper
-            userWin = compChoice ==="paper" ? false : true;
-        } else if(userChoice === "paper") {
-            compChoice === "scissors" ? false : true;
-        } else {
-            compChoice === "rock" ? false : true;
-        }
-        showWinner(userWin, userChoice, compChoice);
+      compChoice === "rock" ? false : true;
     }
-
-
-}
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
 choices.forEach((choice) => {
-    console.log(choice);
+  console.log(choice);
 
-    choice.addEventListener("click", () => {
-        const userChoice = choice.getAttribute("id");
-        console.log("choice was clicked", userChoice);
-        playGame(userChoice)
-
-    });
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    console.log("choice was clicked", userChoice);
+    playGame(userChoice);
+  });
 });
 
 const refreshBtn = document.getElementById("btnRefresh");
